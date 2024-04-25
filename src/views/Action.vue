6679c2eeb5ee1a61d839ed21ed5b89d1932910c0
Daisy Huang <daisy.hmq@outlook.com>
2024-04-25 20:59:09 +0800

    feat: 论坛

--- src/views/Action.vue
+++ src/views/Action.vue
@@ -2,10 +2,13 @@
   <Title color="green"></Title>
   <div class="forum-list">
     <div class="forum-item" v-for="item in forumList" :key="item.id" @click="handleClickNum(item)">
-      <div class="content">{{ item.content }}</div>
-      <div class="click-num">
+      <div class="left-side">
+        <div class="item-id">{{ item.username }}</div>
+        <div class="content">{{ item.content }}</div>
+      </div>
+      <div class="right-side">
         <el-icon><Star /></el-icon>
-        <span style="margin-left: 10px">{{ item.clickNum }}</span>
+        <span class="click-num">{{ item.clickNum }}</span>
       </div>
     </div>
   </div>
@@ -17,61 +20,96 @@
   import { Star } from "@element-plus/icons-vue";
 
   const forumList = ref([
-    {
-      id: 1,
-      content: "论坛1",
-      clickNum: 0
-    },
-    {
-      id: 2,
-      content: "论坛2",
-      clickNum: 9
-    },
-    {
-      id: 3,
-      content: "论坛3",
-      clickNum: 0
-    },
-    {
-      id: 4,
-      content: "论坛4",
-      clickNum: 0
-    },
-    {
-      id: 5,
-      content: "论坛5",
-      clickNum: 0
-    },
-    {
-      id: 6,
-      content: "论坛5",
-      clickNum: 0
-    },  
-    {
-      id: 7,
-      content: "论坛5",
-      clickNum: 0
-    },
-    {
-      id: 8,
-      content: "论坛5",
-      clickNum: 0
-    },
-    {
-      id: 9,
-      content: "论坛5",
-      clickNum: 0
-    },
-    {
-      id: 10,
-      content: "论坛5",
-      clickNum: 0
-    },
-    {
-      id: 11,
-      content: "论坛11",
-      clickNum: 0
-    }
+  {
+    id: 1,
+    username: "赵阳光",
+    content: "绿色环保，美好家园",
+    clickNum: 25
+  },
+  {
+    id: 2,
+    username: "王有才",
+    content: "节约用水，从我做起",
+    clickNum: 18
+  },
+  {
+    id: 3,
+    username: "李晓明",
+    content: "关爱动物，就是关爱我们自己",
+    clickNum: 30
+  },
+  {
+    id: 4,
+    username: "刘一飞",
+    content: "清洁能源，洁净未来",
+    clickNum: 22
+  },
+  {
+    id: 5,
+    username: "孙悟空",
+    content: "蓝天保卫战，我们在行动",
+    clickNum: 14
+  },
+  {
+    id: 6,
+    username: "周小川",
+    content: "垃圾分类，环境优先",
+    clickNum: 19
+  },
+  {
+    id: 7,
+    username: "吴朝旭",
+    content: "保持空气清新，人人有责",
+    clickNum: 17
+  },
+  {
+    id: 8,
+    username: "陈天启",
+    content: "少开一次车，多种一棵树",
+    clickNum: 16
+  },
+  {
+    id: 9,
+    username: "林彤",
+    content: "节能减排，绿色发展",
+    clickNum: 21
+  },
+  {
+    id: 10,
+    username: "何光远",
+    content: "减少塑料使用，拯救海洋生命",
+    clickNum: 24
+  },
+  {
+    id: 11,
+    username: "姚心怡",
+    content: "环保出行，拥抱自然",
+    clickNum: 15
+  },
+  {
+    id: 12,
+    username: "钟离",
+    content: "资源循环利用，让地球更健康",
+    clickNum: 26
+  },
+  {
+    id: 13,
+    username: "马超群",
+    content: "绿色食品，健康生活",
+    clickNum: 20
+  },
+  {
+    id: 14,
+    username: "苏轼",
+    content: "珍惜水资源，保护水环境",
+    clickNum: 23
+  },
+  {
+    id: 15,
+    username: "辛弃疾",
+    content: "每天一小时，地球熄灯行动",
+    clickNum: 12
+  }
   ]);
 
   const handleClickNum = (item) => {
@@ -80,26 +118,40 @@
 </script>
 
 <style scoped>
-  .forum-list {
-    background-color: #f7f7f7;
-    width: 800px;
-    padding: 10px;
-  }
-  .forum-item {
-    background-color: white;
-    padding: 10px;
-    border-radius: 10px;
-    text-align: left;
-    margin-bottom: 10px;
-  }
-  .forum-item:last-child {
-    margin-bottom: 0;
-  }
-  .click-num {
-    text-align: right;
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-    cursor: pointer;
-  }
+.forum-list {
+  background-color: #f7f7f7;
+  width: 800px;
+  padding: 10px;
+}
+.forum-item {
+  padding: 10px;
+  border-radius: 10px;
+  text-align: left;
+  margin-bottom: 10px;
+  display: flex;
+  background-color: white;
+}
+.forum-item:last-child {
+  margin-bottom: 0;
+}
+.left-side {
+  flex-grow: 1;
+}
+.item-id {
+  font-weight: bold;
+}
+.content {
+  margin-top: 4px;
+}
+.right-side {
+  flex-shrink: 0;
+  display: flex;
+  align-items: center;
+  min-width: 100px;
+  justify-content: center;
+}
+.click-num {
+  margin-left: 10px;
+  cursor: pointer;
+}
 </style>
